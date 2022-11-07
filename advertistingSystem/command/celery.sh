d66b392629d836d1343985615275c4d153da8c6b
Ali Nowrouzi <alinowrouzii@gmail.com>
2022-11-07 16:48:47 +0330

    Add celery command

---
+++ advertistingSystem/command/celery.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# cd 
+# source ./venv/bin/activate && celery -A aaic worker --loglevel=INFO --concurrency=8 -n worker1@%h
+
+ 
+ NAME="Advertising Project - celery_worker_start"
+ 
+ PROJECT_DIR=/home/redhat/data/cloud-computing/advertisting-system/advertistingSystem
+ ENV_DIR=/home/redhat/data/cloud-computing/advertisting-system/advertistingSystem/venv/
+ 
+ echo "Starting $NAME as `whoami`"
+ 
+ # Activate the virtual environment
+ cd "${PROJECT_DIR}"
+ 
+ if [ -d "${ENV_DIR}" ]
+ then
+     . "${ENV_DIR}/bin/activate"
+ fi
+
+celery -A advertistingSystem worker --loglevel=INFO --concurrency=2 -n worker1@%h
+
+
+
+# to stop running celery, use this command in django dir
+# celery -A aaic control shutdown
+
+
+# to kill flower
+# pkill -9 -f 'flower'
